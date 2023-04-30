785440b7cd7fa191bc744499f2e3d4c7cc4ca7ac
Serhii Bezuhlyi <kedonosec@gmail.com>
2023-04-30 21:50:35 +0300

    v1.0.2 Add 'hello' command handling

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -5,9 +5,9 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"time"
-	"log"
 
 	"github.com/spf13/cobra"
 	telebot "gopkg.in/telebot.v3"
@@ -20,9 +20,9 @@ var (
 
 // kbotCmd represents the kbot command
 var kbotCmd = &cobra.Command{
-	Use:   "kbot",
+	Use:     "kbot",
 	Aliases: []string{"start"},
-	Short: "A brief description of your command",
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -46,6 +46,17 @@ to quickly create a Cobra application.`,
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
 
 			log.Print(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+
+			log.Print(payload)
+
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello I am Kbot %s", appVersion))
+
+			}
+
+			log.Print(err)
 
 			return err
 
