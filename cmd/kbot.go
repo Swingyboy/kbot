ca46dca3e2ca436072c86aec96d21f5f41871bca
Swingyboy <kedonosec@gmail.com>
2023-06-20 14:57:46 +0000

    update kbot.go

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -50,7 +50,7 @@ to quickly create a Cobra application.`,
 
 			switch payload {
 			case "hello":
-				err = m.Send(fmt.Sprintf("Hello I am Kbot %s", appVersion))
+				err = m.Send(fmt.Sprintf("Hello I am new updated Kbot %s", appVersion))
 
 			}
 
